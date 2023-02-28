8eb74d2edf67b3e7137b1cf364156dec064e7372
Mohammad Mohammadi <imohammadd@gmail.com>
2023-02-28 20:06:43 +0330

    create service provider

---
+++ src/BalinkPayServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace Balink\BalinkPay;
+
+use Illuminate\Support\ServiceProvider;
+
+class BalinkPayServiceProvider extends ServiceProvider {
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
