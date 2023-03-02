cafa74452e4277fb355400501ba845d2e0d59505
Mohammad Mohammadi <imohammadd@gmail.com>
2023-03-02 18:41:14 +0330

    add facade to service provider

--- src/BalinkPayServiceProvider.php
+++ src/BalinkPayServiceProvider.php
@@ -6,6 +6,9 @@ use Illuminate\Support\ServiceProvider;
 class BalinkPayServiceProvider extends ServiceProvider {
     public function register()
     {
+        $this->app->bind('BalinkPay', function($app) {
+            return new BalinkPay();
+        });
         $this->mergeConfigFrom(__DIR__.'/../config/config.php', 'balinkpay');
     }
 
