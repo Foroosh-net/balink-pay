4ca7cbdbdb1634054cbc83e48118f91bd9a32480
Mohammad Mohammadi <imohammadd@gmail.com>
2023-03-02 19:11:30 +0330

    add transaction migration to service provider

--- src/BalinkPayServiceProvider.php
+++ src/BalinkPayServiceProvider.php
@@ -18,6 +18,12 @@ class BalinkPayServiceProvider extends ServiceProvider {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('balinkpay.php'),
             ], 'config');
+
+            if(! class_exists('CreateTransactionTable')) {
+                $this->publishes([
+                    __DIR__ . '../database/migrations/create_transactions_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_transactions_table.php'), 
+                ], 'migrations');
+            }
         }
     }
 }
